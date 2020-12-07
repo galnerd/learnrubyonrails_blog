b892e528aea72963318b2aad50fd3044c28367cc
Galvin Chan <csygalvin@gmail.com>
2020-12-07 22:58:27 +0800

    added destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,15 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
+
 	def new
 		@article = Article.new
 	end
+
 	def edit
 		@article = Article.find(params[:id])
 	end
@@ -21,6 +24,7 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
+
 	def update
 		@article = Article.find(params[:id])
 
@@ -29,6 +33,13 @@ class ArticlesController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	private
